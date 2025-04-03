8f94513daf739f98a3f0ea933c12aa3ac8eea9df
dhaxanxxt <mohn08052006@gmail.com>
2025-04-03 15:55:41 +0530

    Google Analytics Configured

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Github from './components/Github'
-
+import ReactGA from "react-ga4";
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize("G-PGZ5V8X0HX");
+    ReactGA.send("pageview"); 
+  }, []);
   return (
     <div>
       <Hero/>
